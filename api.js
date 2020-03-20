0d1bdbafcb8812cd9b39c1e21c423f12844c9170
Mark Amirkan <markdamirkan@gmail.com>
2020-03-20 13:24:00 -0500

    reformatting of the results page

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 function getAPI() {
 
+  $('#chanceInfected').text(' ');
+  $('#chanceInfected2').text(' ');
   var request = new XMLHttpRequest();
 
   var country = selectCountry.value;
@@ -7,22 +9,37 @@ function getAPI() {
   if (state == '') state = 'blank';
   var theURL = 'https://us-central1-willigetinfectedapi.cloudfunctions.net/getChanceInfected?state=' + state + '&country=' + country + '&days=100';
 
-  request.open('GET', theURL, true); //?state=Texas&country=United%20States%20of%20America&days=100
+  request.open('GET', theURL, true);
   request.onload = function () {
-    // Begin accessing JSON data here
     var data = JSON.parse(this.response);
 
     if (request.status >= 200 && request.status < 400) {
+      if (data.week == 100) var week = 'very high';
+      else var week = data.week + '%';
+      if (data.month == 100) var month = 'very high';
+      else var month = data.month + '%';
+      if (data.eightweek == 100) var eightweek = 'very high';
+      else var eightweek = data.eightweek + '%';
+
       if (state == 'blank') var location = country;
       else var location = state + ', ' + country;
-      if (data.week == null) $('#chanceInfected').text('Please insert a valid location. If the error proceeds, the location you input might not be in our database yet. Check back later.');
-      else if ($('#statediv').css('display') == 'block' && selectState.value == '') $('#chanceInfected').text('Please enter a state/province for that country.');
-      else $('#chanceInfected').text(
-        'In ' + location + ', there are ' + data.confirmed + ' COVID-19 cases out of the total population of ' + data.population + '.\n' +
-        'This means you have a ' + data.week + '% chance of getting infected in the next week, ' +
-        data.month + '% chance in the next month, ' +
-        'and a ' + data.eightweek + '% chance in the next 8 weeks.'
-        );
+
+      if (data.week == null) {
+        $('#chanceInfected').text('Please insert a valid location. If the error proceeds, the location you input might not be in our database yet. Check back later.');
+        $('#chanceInfected2').text('');
+      } else if ($('#statediv').css('display') == 'block' && selectState.value == '') {
+        $('#chanceInfected').text('Please enter a state/province for that country.');
+        $('#chanceInfected2').text('');
+      } else {
+        $('#chanceInfected').text('In ' + location + ', there are ' + data.confirmed + ' COVID-19 cases out of the total population of ' + data.population + '.');
+        $('#chanceInfected2').text('This means you have a:');
+        $('#bValue').text(week);
+        $('#chanceInfected3').text(' chance of getting infected in the next week');
+        $('#bValue2').text(month);
+        $('#chanceInfected4').text(' chance in the next month');
+        $('#bValue3').text(eightweek);
+        $('#chanceInfected5').text(' chance in the next 8 weeks');
+      }
     } else {
       console.log('error');
     }
@@ -42,19 +59,3 @@ function bopemback() {
   selectCountry.value = '';
   selectState.value = '';
 }
-
-
-
-/*
-
-In Washington, United States of America, there are 643 COVID-19 cases out of the total population of 7797095.
-//You have a 7.1557624646% chance of getting infected in the next 100 days. 
-
-According to our calculations, your chance of catching COVID-19 is:
-
-19% within a week
-30% within 2 weeks
-67% withing a month
-
-
-*/
